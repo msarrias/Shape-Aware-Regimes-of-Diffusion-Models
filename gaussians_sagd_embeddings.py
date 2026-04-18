ff48ba6cf5717633ba8f69e77e78db084833bd98
Marina Herrera Sarrias <mahe9165@roos.math.su.se>
2026-04-18 20:53:57 +0200

    refacto to snake case

--- gaussians_sagd_embeddings.py
+++ gaussians_sagd_embeddings.py
@@ -3,32 +3,29 @@ import joblib
 import torch
 from joblib import Parallel, delayed
 from tqdm import tqdm
-from ou_model import forward, backward, score, classify, theoretical_ts
-from plotting import plot_speciation_3d
-from distance_matrix import construct_sagd_distance_matrix
+from ou_model import backward, theoretical_ts
 
 
 from distances import CTD_matrix
 from numpy import isclose
 from stats import normalize, Kruglov_distance
 from scipy.stats import wasserstein_distance
-from SAGD import SAGD
 from adaptive_knn import AdaptiveKNNGraph # comes from graph-theory repo
 from pathlib import Path
 
 
 def ctd_job(
-        W_result: np.ndarray,
+        w_result: np.ndarray,
         laplacian: str,
         normalization: str
 ):
-    C_Gi = CTD_matrix(W=W_result, laplacian_type=laplacian)
-    triu_i = C_Gi[np.triu_indices(W_result.shape[0], k=1)]
+    C_Gi = CTD_matrix(W=w_result, laplacian_type=laplacian)
+    triu_i = C_Gi[np.triu_indices(w_result.shape[0], k=1)]
     
     norm_i = normalize(
         list_values=triu_i,
         norm_type=normalization,
-        Vol=np.sum(W_result)
+        Vol=np.sum(w_result)
     )
     return {'ctds': triu_i, 'norm_ctds': norm_i}
 
@@ -38,26 +35,25 @@ def knn_job(
         inject_edges: bool,
         kernel: str
 ):
-    knn_objt = AdaptiveKNNGraph(
+    knn_obj = AdaptiveKNNGraph(
         data=data,
         inject_edges=inject_edges,
         kernel=kernel
     )
-    W_matrix = knn_objt.compute_W()
-    return knn_obj.k, W_matrix
+    w_matrix = knn_obj.compute_W()
+    return knn_obj.k, w_matrix
 
 
 if __name__ == "__main__":
     torch.manual_seed(123)
-    
     ds = [2, 256, 1024, 4096, 16384]
     T = 10
-    nsteps = 1000
-    nsamples = 1000
-    dt = T / nsteps
+    n_steps = 1000
+    n_samples = 1000
+    dt = T / n_steps
     times = np.arange(0, T, dt)
 
-    nthreads = 16
+    n_threads = 16
     
     for d in ds:
         mu_star = torch.ones(d) * 4
@@ -75,11 +71,11 @@ if __name__ == "__main__":
         if not history_file.exists():
             history = {}
             #  sample for T
-            x_current = torch.randn(d, nsamples)  # d x N
+            x_current = torch.randn(d, n_samples)  # d x N
             history[T] = x_current.T.clone().numpy()  # N x d
             #  T-1 --> 0
             
-            for step in tqdm(reversed(range(nsteps)), total=nsteps, desc=f"SDE D={d}"):
+            for step in tqdm(reversed(range(n_steps)), total=n_steps, desc=f"SDE D={d}"):
                 t = times[step]
                 x_current, _ = backward(x_current, t, dt, mu_star, std)
                 if step in time_indices:
@@ -90,7 +86,7 @@ if __name__ == "__main__":
                 "history": history,
                 "params": {
                     "dim": d,
-                    "nsamples": nsamples,
+                    "n_samples": n_samples,
                     "T_max": T,
                     "dt": dt,
                     "times": times,
@@ -112,11 +108,11 @@ if __name__ == "__main__":
         ws_file = path / f"D{d}_N1000_Ws.jbl"
         
         if not ws_file.exists():
-            W_results, k_results = [], []
+            w_results, k_results = [], []
             inject_edges_bool = True
             kernel_choice = "gaussian"
 
-            knn_results = Parallel(n_jobs=nthreads)(
+            knn_results = Parallel(n_jobs=n_threads)(
                 delayed(knn_job)(
                     history[t], inject_edges_bool, kernel_choice)
                 for t in tqdm(time_snaps, desc="KNN Progress")
@@ -124,14 +120,14 @@ if __name__ == "__main__":
             
             for t, knn_obj in zip(time_snaps, knn_results):
                 k, W = knn_obj
-                W_results.append(W)
+                w_results.append(W)
                 k_results.append(k)
             
-            joblib.dump({"Ws": W_results, "ks": k_results, 'ts': time_snaps}, ws_file, compress=3)
+            joblib.dump({"Ws": w_results, "ks": k_results, 'ts': time_snaps}, ws_file, compress=3)
         else:
             print(f"[D={d}] Weights found. Loading...")
             load_file = joblib.load(ws_file)
-            W_results = load_file["Ws"]
+            w_results = load_file["Ws"]
             time_snaps = load_file['ts']
 
         # CTD Calculation
@@ -140,9 +136,9 @@ if __name__ == "__main__":
             laplacian_type = "unnormalized"
             norm_type = "norm_wrt_avg_ctd"
 
-            ctds = Parallel(n_jobs=nthreads)(
+            ctds = Parallel(n_jobs=n_threads)(
                 delayed(ctd_job)(W_i, laplacian_type, norm_type)
-                for W_i in tqdm(W_results, total=len(W_results), desc="CTD Logic")
+                for W_i in tqdm(w_results, total=len(w_results), desc="CTD Logic")
             )
             ctds_dict = {t: ctd for t, ctd in zip(time_snaps, ctds)}
             ctd_dump = {
@@ -167,14 +163,14 @@ if __name__ == "__main__":
             num_graphs = len(norm_ctds_list)
             pairs = [(i, j) for i in range(num_graphs) for j in range(i + 1, num_graphs)]
 
-            testsize = min(3, num_graphs)
-            for i in range(testsize):
-                for j in range(testsize):
+            test_size = min(3, num_graphs)
+            for i in range(test_size):
+                for j in range(test_size):
                     kruglov_dist = Kruglov_distance(norm_ctds_list[i], norm_ctds_list[j])
                     wasserstein_dist = wasserstein_distance(norm_ctds_list[i], norm_ctds_list[j]) 
                     assert isclose(kruglov_dist, wasserstein_dist)
 
-            distances = Parallel(n_jobs=nthreads)(
+            distances = Parallel(n_jobs=n_threads)(
                 delayed(wasserstein_distance)(norm_ctds_list[i], norm_ctds_list[j])
                 for i, j in tqdm(pairs, desc="Computing SAGD Matrix")
             )
