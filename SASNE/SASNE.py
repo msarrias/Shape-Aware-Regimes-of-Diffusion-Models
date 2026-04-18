670fab47fbd7097241eb765f39816e6d2d031843
Marina Herrera Sarrias <mahe9165@roos.math.su.se>
2026-04-18 21:15:53 +0200

    removed all output messages

--- SASNE/SASNE.py
+++ SASNE/SASNE.py
@@ -6,24 +6,19 @@ from adaptive_knn import AdaptiveKNNGraph
 
 
 def SASNE(data, n_components=2):
-    start_time = time.time()
-    print('Constructing graph...')
     obj_knn = AdaptiveKNNGraph(data)
     W = obj_knn.compute_W() #construct_graph(data)
-    print("--- %s seconds elapsed ---" % round(time.time() - start_time,5))
-    start_time = time.time()
-    print('Computing graph distance...')
     res = get_symbiharmonic_coords(W)
-    print("--- %s seconds elapsed ---" % round(time.time() - start_time,5))
     Z, eigenval = res
     init_Y = 1e-4 * Z[:,[1,2]] * np.sqrt(eigenval[1])
     n = len(W)
     perplexity = 0.9 * n
-    start_time = time.time()
-    print('Computing embedding...')
-    embedding = TSNE(n_components=n_components,init=init_Y,perplexity=perplexity).fit_transform(Z)
-    print("--- %s seconds elapsed ---" % round(time.time() - start_time,5))   
+    embedding = TSNE(
+        n_components=n_components,
+        init=init_Y,
+        perplexity=perplexity
+    ).fit_transform(Z)
 
-    return embedding,Z
+    return embedding, Z
 
 
