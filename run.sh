594a065efa1502cf5271c26c1b065c2116d929e6
Marina Herrera Sarrias <mahe9165@roos.math.su.se>
2026-04-29 14:54:21 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-EXP_NAME="exp_04_fixed_grid"
+EXP_NAME="exp_01"
 THREADS=20
 MU=4.0
 T=10.0
 SAMPLES=1000
 STEPS=1000
-SEED=123456
+SEED=123
 
 KERNEL="gaussian"
 LAPLACIAN="unnormalized"
 NORM="norm_wrt_avg_ctd"
 
-python run_exp.py \
+python gaussians_sagd_embeddings.py \
     --exp_name "$EXP_NAME" \
-    --ds $(seq 2 15) \
+    --ds $(seq 2 6) \
     --threads $THREADS \
     --mu $MU \
     --T $T \
