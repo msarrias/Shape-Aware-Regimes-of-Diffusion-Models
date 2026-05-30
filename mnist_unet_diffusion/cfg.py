e956106b0dff3703fa71ee7d51714e8d5791982b
Marina Herrera Sarrias <marina.sarrias@math.su.se>
2026-05-30 17:19:06 +0200

    fix: update dataset name to MNIST in configuration

--- mnist_unet_diffusion/cfg.py
+++ mnist_unet_diffusion/cfg.py
@@ -9,14 +9,14 @@ def load_config(DATASET):
     config.N_STEPS = 150000 #int(5e5)+1
     config.path_save = '../Saves/'
     
-    if DATASET == 'mnist_unet_diffusion':
+    if DATASET == 'MNIST':
         config.IMG_SHAPE = (1, 32, 32)
         config.BATCH_SIZE = 128
         config.path_data = '../data/'
         config.CENTER = True
         config.STANDARDIZE = False
         config.n_images = 15000 #10000
-        config.dataset_params = {"name": "mnist_unet_diffusion", 
+        config.dataset_params = {"name": "MNIST", 
                                  "classes": [0, 1, 8],
                                  "props": [1/3, 1/3, 1/3]}
     else:
