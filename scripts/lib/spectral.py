6d2f0bd85dd5faa2d6a450c00e1ee22e41acbd4e
Marina Herrera Sarrias <marina.sarrias@math.su.se>
2026-07-03 10:13:22 +0200

    remove normalization functions from spectral.py to stats.py

--- scripts/lib/spectral.py
+++ scripts/lib/spectral.py
@@ -30,32 +30,3 @@ def solve_and_sort_std_eigv_problem(matrix):
     eigvc = eigvc[:, idx]  # column-wise
 
     return eigv, eigvc
-
-
-def scale_and_shift(arr):
-    v_min, v_max = arr.min(), arr.max()
-    return (arr - v_min) / (v_max - v_min) if v_max != v_min else arr
-
-
-def normalize(list_values, norm_type, Vol=None):
-    arr = np.asarray(list_values)
-    if norm_type == "scale_and_shift":
-        return scale_and_shift(arr)
-
-    if norm_type == "log_scale_and_shift":
-        log_ctd = np.log1p(arr)
-        return scale_and_shift(log_ctd)
-
-    if norm_type == "norm_wrt_volume":
-        if Vol is None or Vol == 0:
-            raise ValueError("Vol must be provided and non-zero for 'norm_wrt_volume'")
-        return arr / Vol
-
-    if norm_type == "norm_wrt_avg_ctd":
-        arr_mean = np.mean(arr)
-        if arr_mean == 0:
-            raise ValueError("Mean of input values must be non-zero for 'norm_wrt_avg_ctd'")
-        return arr / arr_mean
-
-    raise ValueError("Unsupported norm_type")
-    
---
+++ scripts/lib/stats.py
@@ -0,0 +1,53 @@
+import numpy as np
+from collections import Counter
+from scipy.stats import rankdata
+
+
+def scale_and_shift(
+        arr: np.ndarray,
+):
+    v_min, v_max = arr.min(), arr.max()
+    return (arr - v_min) / (v_max - v_min) if v_max != v_min else arr
+
+
+def normalize(
+        list_values: list,
+        norm_type: str,
+        global_list_values: np.ndarray | None,
+        clipping: bool = False,
+        clip_percentile: float = 95
+):
+    arr = np.asarray(list_values)
+
+    if norm_type == "log_global_scale_and_shift":
+        log_arr = np.log1p(arr)
+
+        if clipping:
+            upper = np.percentile(global_list_values, clip_percentile)
+            lower = np.percentile(global_list_values, 100 - clip_percentile)
+            log_arr = np.clip(log_arr, lower, upper)
+        else:
+            lower = np.min(log_arr)
+            upper = np.max(log_arr)
+
+        return (log_arr - lower) / (upper - lower)
+
+    if clipping:
+        upper = np.percentile(arr, clip_percentile)
+        lower = np.percentile(arr, 100 - clip_percentile)
+        arr = np.clip(arr, lower, upper)
+
+    if norm_type == "scale_and_shift":
+        return scale_and_shift(arr)
+
+    elif norm_type == "log_scale_and_shift":
+        log_ctd = np.log1p(arr)
+        return scale_and_shift(log_ctd)
+
+    elif norm_type == "norm_wrt_volume":
+        return arr / np.sum(arr)
+
+    elif norm_type == "norm_wrt_avg_ctd":
+        return arr / np.mean(arr)
+
+    raise ValueError("Unsupported norm_type")
